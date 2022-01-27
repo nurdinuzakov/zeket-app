51b2197b3b19e7e7852df85fa254eeb955892ec0
nurdinuzakov <nurdinuzakov@gmail.com>
2022-01-27 22:55:32 +0600

    added Posts image and comments migrations

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
 Route::get('/not-authorized',[AuthController::class, 'notAuthorized'])->name('not_authorized');
 
 Route::get('/auth/google', function () {
